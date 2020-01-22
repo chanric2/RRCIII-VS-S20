639d00e656eb35267e06fbbfd44db041f2f7247f
jekasku chandler <jekasku@gmail.com>
2020-01-22 08:04:38 -0700

    Created SayMyName

---
+++ SayMyName/Module1.vb
@@ -0,0 +1,23 @@
+﻿'Richard Chandler III
+'RCET0265
+'Spring 2020
+'Say My Name
+'https://github.com/chanric2/TJR-VS-S20
+
+Module Module1
+
+    Sub Main()
+
+        Dim userMessage As String
+
+        userMessage = "Name"
+
+        Console.WriteLine("Please Enter Your Name. Press Enter When Done")
+        userMessage = Console.ReadLine()
+
+        Console.WriteLine("Hello " & Chr(34) & userMessage & Chr(34) & " You May Now Use This Device!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
