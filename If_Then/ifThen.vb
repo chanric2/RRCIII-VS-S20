9e26a567ba01a24541fd9ec417b370bca78ea848
jekasku chandler <jekasku@gmail.com>
2020-01-23 08:40:59 -0700

    Added more conitions as well as user selected variables.

--- If_Then/ifThen.vb
+++ If_Then/ifThen.vb
@@ -8,11 +8,31 @@ Module IfThen
 
     Sub Main()
 
-        If 1 < 2 Then
-            Console.WriteLine("True")
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+
+        'This section of code below is to allow the user to type in the integer instead of set values
+        Console.WriteLine("Select first number")
+        firstNumber = Console.ReadLine()
+
+        Console.WriteLine("Select second number")
+        secondNumber = Console.ReadLine()
+
+        'This selction of code below is to compare the values. Based on the first true condition the code will end.
+        If firstNumber > secondNumber Then
+            Console.WriteLine("First number is bigger than the second.")
+        ElseIf firstNumber < secondNumber Then
+            Console.WriteLine("Second number is bigger than the first.")
+        ElseIf firstNumber <= secondNumber Then
+            Console.WriteLine("First number is less than or equal to the second.")
+        ElseIf firstNumber >= secondNumber Then
+            Console.WriteLine("First number is greater than or equal to the second.")
+        ElseIf firstNumber <> secondNumber Then
+            Console.WriteLine("not equal")
         Else
-            Console.WriteLine("False")
+            Console.WriteLine("The numbers are equal.")
         End If
+        Console.ReadLine()
 
     End Sub
 
