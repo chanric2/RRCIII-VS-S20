ac0306eed62f2629428aa077e6242e3268446955
jekasku chandler <jekasku@gmail.com>
2020-01-23 08:50:52 -0700

    created a second conditional to run after and display BOTH outputs

--- If_Then/ifThen.vb
+++ If_Then/ifThen.vb
@@ -18,12 +18,17 @@ Module IfThen
         Console.WriteLine("Select second number")
         secondNumber = Console.ReadLine()
 
-        'This selction of code below is to compare the values. Based on the first TRUE condition the code will end.
+        'This selction of code below is to compare the values. Based on the first TRUE condition the code will continue.
         If firstNumber > secondNumber Then
             Console.WriteLine("First number is bigger than the second.")
         ElseIf firstNumber < secondNumber Then
             Console.WriteLine("Second number is bigger than the first.")
-        ElseIf firstNumber <= secondNumber Then
+        ElseIf firstNumber = secondNumber Then
+            Console.WriteLine("The numbers are the same.")
+        End If
+
+        'A second conditional to run after the first. Based on the first TRUE condition the code will end.
+        If firstNumber <= secondNumber Then
             Console.WriteLine("First number is less than or equal to the second.")
         ElseIf firstNumber >= secondNumber Then
             Console.WriteLine("First number is greater than or equal to the second.")
@@ -32,6 +37,7 @@ Module IfThen
         Else
             Console.WriteLine("The numbers are equal.")
         End If
+        'This line is to display the codes output answer of BOTH conditions
         Console.ReadLine()
 
     End Sub
