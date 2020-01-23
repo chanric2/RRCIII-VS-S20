3209d008812cbb81f847b1941e3ae6b5be59dce7
jekasku chandler <jekasku@gmail.com>
2020-01-23 08:22:22 -0700

    create If_Then statement

---
+++ If_Then/ifThen.vb
@@ -0,0 +1,19 @@
+﻿'Richard Chandler III
+'RCET0265
+'Spring 2020
+'If_Then
+'https://github.com/chanric2/RRCIII-VS-S20
+
+Module IfThen
+
+    Sub Main()
+
+        If 1 < 2 Then
+            Console.WriteLine("True")
+        Else
+            Console.WriteLine("False")
+        End If
+
+    End Sub
+
+End Module
