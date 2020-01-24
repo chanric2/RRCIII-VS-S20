166f3e2b6e5288d2fc5d85dbc9c6f2c6b9cfcbc9
jekasku chandler <jekasku@gmail.com>
2020-01-24 08:56:25 -0700

    Code in progress OR "Joe" fails

--- SayUserMessage/SayUserMessage.vb
+++ SayUserMessage/SayUserMessage.vb
@@ -4,22 +4,22 @@
 'Say User Message
 'https://github.com/chanric2/RRCIII-VS-S20
 
+'option compare binary is default and compare all characters in ASCII if they do not match it fails
+'option compare text will set the ASCII J = j Character by character so case wont matter
+Option Compare Text
+
 Module SayUserMessage
 
     Sub Main()
 
         Dim username As String
 
-        Console.WriteLine("Hello, To Get Started Please Enter Your Name. Then Hit Enter.")
+        Console.WriteLine("Hello, To Get Started Please Enter Your Username. Then Hit Enter.")
         username = Console.ReadLine()
 
-        If username = "Emily" Then
-            Console.WriteLine("Welcome Emily To Our Facility!")
-        ElseIf username = "Joe" Then
-            Console.WriteLine("Welcome Joe To Our Location!")
-        ElseIf username <> "Emily" Then
-            Console.WriteLine("You Do Not Have Access, Please Leave Now Or We Will Call The Police.")
-        ElseIf username <> "Joe" Then
+        If username = "Emily" Or "Joe" Then
+            Console.WriteLine("Welcome " & username & " To Our Facility!")
+        ElseIf username <> "Emily" Or username <> "Joe" Then
             Console.WriteLine("You Do Not Have Access, Please Leave Now Or We Will Call The Police.")
         End If
 
