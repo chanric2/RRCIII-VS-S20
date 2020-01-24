ed0457a6fce79d113b8e066b5b1cd51b9e227109
jekasku chandler <jekasku@gmail.com>
2020-01-24 08:21:44 -0700

    Created a code to login to a facility.

---
+++ MathWithComparisons/Module1.vb
@@ -0,0 +1,13 @@
+﻿'Richard Chandler III
+'RCET0265
+'Spring 2020
+'Math With Comparisons
+'
+
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
---
+++ SayUserMessage/SayUserMessage.vb
@@ -0,0 +1,30 @@
+﻿'Richard Chandler III
+'RCET0265
+'Spring 2020
+'Say User Message
+'https://github.com/chanric2/RRCIII-VS-S20
+
+Module SayUserMessage
+
+    Sub Main()
+
+        Dim username As String
+
+        Console.WriteLine("Hello, To Get Started Please Enter Your Name. Then Hit Enter.")
+        username = Console.ReadLine()
+
+        If username = "Emily" Then
+            Console.WriteLine("Welcome Emily To Our Facility!")
+        ElseIf username = "Joe" Then
+            Console.WriteLine("Welcome Joe To Our Location!")
+        ElseIf username <> "Emily" Then
+            Console.WriteLine("You Do Not Have Access, Please Leave Now Or We Will Call The Police.")
+        ElseIf username <> "Joe" Then
+            Console.WriteLine("You Do Not Have Access, Please Leave Now Or We Will Call The Police.")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
