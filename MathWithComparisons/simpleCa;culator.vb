73df24971490c071189221bae9c8b07e5c4505c8
jekasku chandler <jekasku@gmail.com>
2020-01-25 03:22:30 -0700

    added more visually appealing user interface changes.

--- MathWithComparisons/simpleCa;culator.vb
+++ MathWithComparisons/simpleCa;culator.vb
@@ -16,28 +16,31 @@ Module Module1
         Dim secondNumber As Integer
 
         'prompt user for number, operation, and final number
-        Console.WriteLine("Select an integer, 0-9. Press ENTER when done.")
+        Console.WriteLine("Select an integer. Press ENTER when done.")
         firstNumber = CInt(Console.ReadLine())
         Console.WriteLine("Select a math operation by typing the number associated to it; 1.Add, 2.Subtract, 3.Multiply, 4.Divide. Press ENTER when done.")
         userOperation = CInt(Console.ReadLine())
-        Console.WriteLine("Select an integer, 0-9. Press ENTER when done.")
+        Console.WriteLine("Select an integer. Press ENTER when done.")
         secondNumber = CInt(Console.ReadLine())
 
-        'select operator from user value
+        'clear the user selected information from console before running the math and displaying the answer
+        Console.Clear()
+
+        'performs the operation choosen by user with the integers choosen by users and displays the answer
         If userOperation = 1 Then
-            Console.WriteLine(firstNumber & "+" & secondNumber & "=" & firstNumber + secondNumber)
+            Console.WriteLine(firstNumber & " + " & secondNumber & " = " & firstNumber + secondNumber)
 
         ElseIf userOperation = 2 Then
-            Console.WriteLine(firstNumber & "-" & secondNumber & "=" & firstNumber - secondNumber)
+            Console.WriteLine(firstNumber & " - " & secondNumber & " = " & firstNumber - secondNumber)
 
         ElseIf userOperation = 3 Then
-            Console.WriteLine(firstNumber & "X" & secondNumber & "=" & firstNumber * secondNumber)
+            Console.WriteLine(firstNumber & " * " & secondNumber & " = " & firstNumber * secondNumber)
 
         ElseIf userOperation = 4 Then
-            Console.WriteLine(firstNumber & "/" & secondNumber & "=" & firstNumber / secondNumber)
+            Console.WriteLine(firstNumber & " / " & secondNumber & " = " & firstNumber / secondNumber)
 
         ElseIf userOperation <> 1 Or userOperation <> 2 Or userOperation <> 3 Or userOperation <> 4 Then
-            Console.WriteLine("Integer OR Operation was INVALID. Please Try Again.")
+            Console.WriteLine("Operation was INVALID. Please Try Again.")
 
         End If
 
