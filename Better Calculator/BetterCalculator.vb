79476bdf57fa6897c80e9e83272d56fbce80361c
jekasku chandler <jekasku@gmail.com>
2020-02-07 08:56:42 -0700

    full loop for numberOne and numberTwo

--- Better Calculator/BetterCalculator.vb
+++ Better Calculator/BetterCalculator.vb
@@ -12,13 +12,14 @@ Module BetterCalculator
 
     Sub Main()
         Dim numberOne As String
-        ' Dim numberTwo As String
+        Dim numberTwo As String
         Dim userReprompt As Boolean
 
         userReprompt = True
 
         Console.WriteLine("Please enter two numbers. Enter 'Q' at any time to quit")
 
+        'choose first number
         Console.WriteLine("Choose a number:")
         Do While userReprompt = True
             Try
@@ -26,9 +27,25 @@ Module BetterCalculator
                 numberOne = CInt(numberOne)
                 userReprompt = False
             Catch ex As Exception
-                userReprompt = False
+                userReprompt = True
                 Console.Clear()
                 Console.WriteLine("You entered " & Chr(34) & numberOne & Chr(34) & ", please enter a whole number.")
+            End Try
+        Loop
+
+        userReprompt = True
+
+        'choose second number
+        Console.WriteLine("Choose a number:")
+        Do While userReprompt = True
+            Try
+                numberTwo = Console.ReadLine()
+                numberTwo = CInt(numberTwo)
+                userReprompt = False
+            Catch ex As Exception
+                userReprompt = False
+                Console.Clear()
+                Console.WriteLine("You entered " & Chr(34) & numberTwo & Chr(34) & ", please enter a whole number.")
             End Try
         Loop
 
@@ -36,17 +53,11 @@ Module BetterCalculator
 
 
 
-        'Console.WriteLine("Choose a number:")
-        'numberTwo = CInt(Console.ReadLine())
 
 
 
 
 
-
-
-
-        Console.WriteLine(numberOne) '& numberTwo)
         Console.ReadLine()
     End Sub
 
