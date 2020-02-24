d9237b8ec192828a436f22c4c4f93beed6552c00
jekasku chandler <jekasku@gmail.com>
2020-02-24 09:03:13 -0700

    updated from tim's github

--- List Form/ListForm.vb
+++ List Form/ListForm.vb
@@ -1,6 +1,23 @@
 ﻿Public Class ListForm
-    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Me.Load
-
+    Private Sub ListBox1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FirstTextBox.Text = "First Name"
+        LastTextBox.Text = "Last Name"
     End Sub
 
+    Private Sub AddButton_Click(sender As Object, e As EventArgs) Handles AddButton.Click
+        NameListBox.Items.Add(FirstTextBox.Text & " " & LastTextBox.Text)
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        NameListBox.Items.Clear()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub NameListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles NameListBox.SelectedIndexChanged
+        Me.Text = NameListBox.SelectedIndex.ToString
+        'TODO add selected to first name and last name text box
+    End Sub
 End Class
