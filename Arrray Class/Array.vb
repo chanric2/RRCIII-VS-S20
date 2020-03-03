53005118ed3341c7397fd7e7cd455dc19fb34213
jekasku chandler <jekasku@gmail.com>
2020-03-03 08:35:09 -0700

    array example

--- Arrray Class/Array.vb
+++ Arrray Class/Array.vb
@@ -6,8 +6,18 @@
 Module Array
 
     Sub Main()
-        Dim myArray(3, 3) As Integer
+        '4 x 4 Array
+        'Dim myArray(3, 3) As Integer
+        'myArray(1, 1) = 5
 
+        'single dim array 0-3
+        Dim myArray(3) As Integer
+        myArray = {1, 2, 3, 4}
+        'sets 0 index to integer 5
+        myArray(0) = 5
+        'save Data(preserve) in array And change size to 0-15 (redim)
+        ReDim Preserve myArray(15)
+        Console.ReadLine()
     End Sub
 
 End Module
