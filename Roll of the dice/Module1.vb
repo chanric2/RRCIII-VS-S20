f4625a24d7fba1f023a0e131cfa51649227293c4
jekasku chandler <jekasku@gmail.com>
2020-03-09 08:57:53 -0600

    check each value resut and write the roll

--- Roll of the dice/Module1.vb
+++ Roll of the dice/Module1.vb
@@ -9,12 +9,43 @@
         Randomize()
         diceTwo = CInt((5 * Rnd()) + 1)
 
-        Dim results(1000) As Integer
+        Dim results(10) As Integer
         For Each roll As Integer In results
-            roll = diceOne + diceTwo
+            If diceOne + diceTwo = 2 Then
+                results(1) += 1
+            End If
+            If diceOne + diceTwo = 3 Then
+                results(2) += 1
+            End If
+            If diceOne + diceTwo = 4 Then
+                results(3) += 1
+            End If
+            If diceOne + diceTwo = 5 Then
+                results(4) += 1
+            End If
+            If diceOne + diceTwo = 6 Then
+                results(5) += 1
+            End If
+            If diceOne + diceTwo = 7 Then
+                results(6) += 1
+            End If
+            If diceOne + diceTwo = 8 Then
+                results(7) += 1
+            End If
+            If diceOne + diceTwo = 9 Then
+                results(8) += 1
+            End If
+            If diceOne + diceTwo = 10 Then
+                results(9) += 1
+            End If
+            If diceOne + diceTwo = 11 Then
+                results(10) += 1
+            End If
+            If diceOne + diceTwo = 12 Then
+                results(11) += 1
+            End If
             Console.WriteLine(roll)
         Next
-
         Console.ReadLine()
     End Sub
 
