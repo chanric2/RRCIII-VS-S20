ada837d2cb15d4e201e310cce2f54e92ac959245
jekasku chandler <jekasku@gmail.com>
2020-03-10 08:53:24 -0600

    Done

--- Roll of the dice/Module1.vb
+++ Roll of the dice/Module1.vb
@@ -3,49 +3,64 @@
     Sub Main()
 
         Dim diceOne As Integer
-        Randomize()
-        diceOne = CInt((5 * Rnd()) + 1)
         Dim diceTwo As Integer
-        Randomize()
-        diceTwo = CInt((5 * Rnd()) + 1)
+        Dim sumDice As Integer
+        Dim diceArray(12, 1) As Integer
 
-        Dim results(10) As Integer
-        For Each roll As Integer In results
-            If diceOne + diceTwo = 2 Then
-                results(1) += 1
+        For roll = 1 To 1000
+            diceOne = Int((6 * Rnd()) + 1)
+            diceTwo = Int((6 * Rnd()) + 1)
+            sumDice = diceOne + diceTwo
+
+            If sumDice = 2 Then
+                diceArray(2, 1) += 1
+            ElseIf sumDice = 3 Then
+                diceArray(3, 1) += 1
+            ElseIf sumDice = 4 Then
+                diceArray(4, 1) += 1
+            ElseIf sumDice = 5 Then
+                diceArray(5, 1) += 1
+            ElseIf sumDice = 6 Then
+                diceArray(6, 1) += 1
+            ElseIf sumDice = 7 Then
+                diceArray(7, 1) += 1
+            ElseIf sumDice = 8 Then
+                diceArray(8, 1) += 1
+            ElseIf sumDice = 9 Then
+                diceArray(9, 1) += 1
+            ElseIf sumDice = 10 Then
+                diceArray(10, 1) += 1
+            ElseIf sumDice = 12 Then
+                diceArray(11, 1) += 1
+            ElseIf sumDice = 11 Then
+                diceArray(12, 1) += 1
             End If
-            If diceOne + diceTwo = 3 Then
-                results(2) += 1
-            End If
-            If diceOne + diceTwo = 4 Then
-                results(3) += 1
-            End If
-            If diceOne + diceTwo = 5 Then
-                results(4) += 1
-            End If
-            If diceOne + diceTwo = 6 Then
-                results(5) += 1
-            End If
-            If diceOne + diceTwo = 7 Then
-                results(6) += 1
-            End If
-            If diceOne + diceTwo = 8 Then
-                results(7) += 1
-            End If
-            If diceOne + diceTwo = 9 Then
-                results(8) += 1
-            End If
-            If diceOne + diceTwo = 10 Then
-                results(9) += 1
-            End If
-            If diceOne + diceTwo = 11 Then
-                results(10) += 1
-            End If
-            If diceOne + diceTwo = 12 Then
-                results(11) += 1
-            End If
-            Console.WriteLine(roll)
         Next
+
+        diceArray(2, 0) = "2"
+        diceArray(3, 0) = "3"
+        diceArray(4, 0) = "4"
+        diceArray(5, 0) = "5"
+        diceArray(6, 0) = "6"
+        diceArray(7, 0) = "7"
+        diceArray(8, 0) = "8"
+        diceArray(9, 0) = "9"
+        diceArray(10, 0) = "10"
+        diceArray(11, 0) = "11"
+        diceArray(12, 0) = "12"
+
+        Console.WriteLine("Dice Possibilities " & " Times Dice Rolled")
+        Console.WriteLine(diceArray(2, 0) & "                     " & diceArray(2, 1))
+        Console.WriteLine(diceArray(3, 0) & "                     " & diceArray(3, 1))
+        Console.WriteLine(diceArray(4, 0) & "                     " & diceArray(4, 1))
+        Console.WriteLine(diceArray(5, 0) & "                     " & diceArray(5, 1))
+        Console.WriteLine(diceArray(6, 0) & "                     " & diceArray(6, 1))
+        Console.WriteLine(diceArray(7, 0) & "                     " & diceArray(7, 1))
+        Console.WriteLine(diceArray(8, 0) & "                     " & diceArray(8, 1))
+        Console.WriteLine(diceArray(9, 0) & "                     " & diceArray(9, 1))
+        Console.WriteLine(diceArray(10, 0) & "                    " & diceArray(10, 1))
+        Console.WriteLine(diceArray(11, 0) & "                    " & diceArray(11, 1))
+        Console.WriteLine(diceArray(12, 0) & "                    " & diceArray(12, 1))
         Console.ReadLine()
     End Sub
 
