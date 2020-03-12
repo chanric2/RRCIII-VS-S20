c49af2504eb1bed304fe0235ce032a9384fc53a8
jekasku chandler <jekasku@gmail.com>
2020-03-12 09:10:18 -0600

    finished listbox

---
+++ RollingForm/RollingForm.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RollingForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ResListBox = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ResListBox
+        '
+        Me.ResListBox.FormattingEnabled = True
+        Me.ResListBox.Location = New System.Drawing.Point(12, 12)
+        Me.ResListBox.Name = "ResListBox"
+        Me.ResListBox.Size = New System.Drawing.Size(776, 342)
+        Me.ResListBox.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(12, 360)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(190, 78)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "&ROLL"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(404, 360)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(190, 78)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&CLEAR"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(598, 360)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(190, 78)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&XIT"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RollingForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.ResListBox)
+        Me.Name = "RollingForm"
+        Me.Text = "Dice Rolling"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ResListBox As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RollingForm/RollingForm.vb
@@ -0,0 +1,56 @@
+﻿'Richard Chandler III
+'RCET0265
+'Spring 2020
+'Rolling Form
+'https://github.com/chanric2/RRCIII-VS-S20
+
+Public Class RollingForm
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        Dim diceOne As Integer
+        Dim diceTwo As Integer
+        Dim sumDice As Integer
+        Dim array(1, 12) As Integer
+        Dim header As String
+        Dim count As String
+
+        For roll = 1 To 1000
+            diceOne = Int((6 * Rnd()) + 1)
+            diceTwo = Int((6 * Rnd()) + 1)
+            sumDice = diceOne + diceTwo
+            If sumDice = 2 Then
+                array(1, 2) += 1
+            ElseIf sumDice = 3 Then
+                array(1, 3) += 1
+            ElseIf sumDice = 4 Then
+                array(1, 4) += 1
+            ElseIf sumDice = 5 Then
+                array(1, 5) += 1
+            ElseIf sumDice = 6 Then
+                array(1, 6) += 1
+            ElseIf sumDice = 7 Then
+                array(1, 7) += 1
+            ElseIf sumDice = 8 Then
+                array(1, 8) += 1
+            ElseIf sumDice = 9 Then
+                array(1, 9) += 1
+            ElseIf sumDice = 10 Then
+                array(1, 10) += 1
+            ElseIf sumDice = 11 Then
+                array(1, 11) += 1
+            ElseIf sumDice = 12 Then
+                array(1, 12) += 1
+            End If
+        Next
+        header = "2   |  3   |  4   |  5   |   6   |  7   |   8   |  9   | 10 | 11 | 12 |"
+        count = array(1, 2) & " |  " & array(1, 3) & " |  " & array(1, 4) & " |" & array(1, 5) & " | " & array(1, 6) & " |" & array(1, 7) & " | " & array(1, 8) & " |" & array(1, 9) & " | " & array(1, 10) & " | " & array(1, 11) & " | " & array(1, 12) & " |"
+        ResListBox.Items.Add(header)
+        ResListBox.Items.Add(count)
+    End Sub
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ResListBox.Items.Clear()
+    End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
