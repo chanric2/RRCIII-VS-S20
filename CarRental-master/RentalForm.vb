c88f818de466c244248eafce9fd4626732ccbd86
jekasku chandler <jekasku@gmail.com>
2020-04-13 21:24:29 -0600

    Redo initialization

--- CarRental-master/RentalForm.vb
+++ CarRental-master/RentalForm.vb
@@ -8,99 +8,94 @@ Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
-    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        ResetAll()
-        SummaryButton.Enabled = False
-    End Sub
-
-    Sub ResetAll()
-        'Clear User input
-        NameTextBox.Text = ""
-        AddressTextBox.Text = ""
-        CityTextBox.Text = ""
-        StateTextBox.Text = ""
-        ZipCodeTextBox.Text = ""
-        BeginOdometerTextBox.Text = ""
-        EndOdometerTextBox.Text = ""
-        DaysTextBox.Text = ""
-        MilesradioButton.Select()
-        AAAcheckbox.Checked = False
-        Seniorcheckbox.Checked = False
-        'Clear Output
-
-    End Sub
-
-    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
-        ResetAll()
-    End Sub
-
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
-        'place sub calls
-        Discount(500)
-        MilageCharge(220)
-        verifyOdometer()
-        MsgBox(UserMessages(False, "", False))
+        If ValidText() = True Then
+            'calculate information
+            MsgBox("Pass Vaild") 'this will be taken out once all vaildations can be confirmed
+        End If
     End Sub
-
-    Function Discount(totalCharges As Decimal) As Decimal
-        Const AAARATE = 0.05D
-        Const SENIORRATE = 0.03D
-        Dim totalDiscount As Decimal = 0
-
-        If AAAcheckbox.Checked = True Then
-            totalDiscount += totalCharges * AAARATE
-        End If
-
-        If Seniorcheckbox.Checked = True Then
-            totalDiscount += totalCharges * SENIORRATE
-        End If
-
-        Return totalDiscount
-    End Function
-
-    Sub verifyOdometer()
-        Dim userMessage As String
-
+    Function ValidText() As Boolean
+        'check empty text and if values are in tolerance.
+        Dim valid As Boolean
         Try
-            If CDec(BeginOdometerTextBox.Text) > CDec(EndOdometerTextBox.Text) Then
-                userMessage = "Invalid Beginning Odometer Number"
+            If NameTextBox.Text <> "" And AddressTextBox.Text <> "" And
+                CityTextBox.Text <> "" And StateTextBox.Text <> "" And
+                ZipCodeTextBox.Text <> "" And BeginOdometerTextBox.Text <> "" And
+                EndOdometerTextBox.Text <> "" And DaysTextBox.Text <> "" And
+                CInt(BeginOdometerTextBox.Text) < CInt(EndOdometerTextBox.Text) And
+                (CInt(DaysTextBox.Text) > 0 Or CInt(DaysTextBox.Text) < 45) Then
+                'true "if all text fields are filled"
+                valid = True
+            ElseIf NameTextBox.Text = "" And AddressTextBox.Text = "" And
+                CityTextBox.Text = "" And StateTextBox.Text = "" And
+                ZipCodeTextBox.Text = "" And BeginOdometerTextBox.Text = "" And
+                EndOdometerTextBox.Text = "" And DaysTextBox.Text = "" And
+                CInt(BeginOdometerTextBox.Text) > CInt(EndOdometerTextBox.Text) And
+                (CInt(DaysTextBox.Text) < 0 Or CInt(DaysTextBox.Text) > 45) Then
+                valid = False
             End If
+
         Catch ex As Exception
-            userMessage = "Odometer entry must be numbers"
+            'false "A text field is Invalid"
+            valid = False
         End Try
-        UserMessages(True, userMessage, False)
-    End Sub
-
-    Function MilageCharge(ByRef miles As Decimal) As Decimal
-        Dim mileCharge As Decimal
-
-        Const RATE_REGULAR = 0.12D
-        Const RATE_LOW = 0.1D
-        Const RATE_FREE = 0D
-
-        Select Case miles
-            Case <= 200
-                mileCharge = miles * RATE_FREE
-            Case > 500
-                mileCharge = miles * RATE_LOW
-            Case Else
-                mileCharge = miles * RATE_REGULAR
-        End Select
-
-        Return mileCharge
-    End Function
-
-    Function UserMessages(addMessage As Boolean, message As String, clearMessage As Boolean) As String
-        Static formattedMessages As String
-
-        If clearMessage = True Then
-            formattedMessages = ""
-        ElseIf addMessage = True Then
-            formattedMessages &= message & vbNewLine
+        'if empty sends message and sets cursor to the box.
+        If NameTextBox.Text = "" Then
+            NameTextBox.Focus()
+            MsgBox(FailMessage("Name is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If AddressTextBox.Text = "" Then
+            AddressTextBox.Focus()
+            MsgBox(FailMessage("Address is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If CityTextBox.Text = "" Then
+            CityTextBox.Focus()
+            MsgBox(FailMessage("City is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If StateTextBox.Text = "" Then
+            StateTextBox.Focus()
+            MsgBox(FailMessage("State is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If ZipCodeTextBox.Text = "" Then
+            ZipCodeTextBox.Focus()
+            MsgBox(FailMessage("Zip Code is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If BeginOdometerTextBox.Text = "" Then
+            BeginOdometerTextBox.Focus()
+            MsgBox(FailMessage("Beginning Odometer Number is blank. Please Fill Out Form Completely.",
+                               True, False))
+        End If
+        If EndOdometerTextBox.Text = "" Then
+            EndOdometerTextBox.Focus()
+            MsgBox(FailMessage("End Odometer Number is blank. Please Fill Out Form Completely.", True, False))
+        End If
+        If DaysTextBox.Text = "" Then
+            DaysTextBox.Focus()
+            MsgBox(FailMessage("Numbers of days is blank. Please Fill Out Form Completely.", True, False))
         End If
 
-        Return formattedMessages
-
+        Return valid
     End Function
 
+    Function FailMessage(errMessage As String, addMessage As Boolean, clearMessage As Boolean) As String
+        Dim alertMessage As String
+        alertMessage = ""
+        If clearMessage = False Then
+            If addMessage = True Then
+                alertMessage &= errMessage & vbNewLine
+            End If
+        Else
+            alertMessage &= ""
+        End If
+
+        Return alertMessage
+    End Function
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Dim response = MsgBox("Click YES to Quit." & vbNewLine & "Click NO to Continue.", MsgBoxStyle.YesNo _
+                              Or MsgBoxStyle.DefaultButton2 Or MsgBoxStyle.Exclamation,
+                              "Are You Sure You Want To QUIT?")
+        If response = MsgBoxResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
 End Class
