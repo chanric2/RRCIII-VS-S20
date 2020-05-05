b9c5dc7ce280bac41a204d14c622c497d42799a9
jekasku chandler <jekasku@gmail.com>
2020-05-05 13:44:56 -0600

    start basis for drawing usng mouse. setting pen colors erasing graphic TOFIX connect controls to buttons

---
+++ Etch And Sketch/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GraphicForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ColorButton = New System.Windows.Forms.Button()
+        Me.EraseButton = New System.Windows.Forms.Button()
+        Me.ColorChooser = New System.Windows.Forms.ColorDialog()
+        Me.SuspendLayout()
+        '
+        'ColorButton
+        '
+        Me.ColorButton.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ColorButton.Location = New System.Drawing.Point(12, 365)
+        Me.ColorButton.Name = "ColorButton"
+        Me.ColorButton.Size = New System.Drawing.Size(235, 73)
+        Me.ColorButton.TabIndex = 0
+        Me.ColorButton.Text = "Color"
+        Me.ColorButton.UseVisualStyleBackColor = True
+        '
+        'EraseButton
+        '
+        Me.EraseButton.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EraseButton.Location = New System.Drawing.Point(553, 365)
+        Me.EraseButton.Name = "EraseButton"
+        Me.EraseButton.Size = New System.Drawing.Size(235, 73)
+        Me.EraseButton.TabIndex = 1
+        Me.EraseButton.Text = "Erase"
+        Me.EraseButton.UseVisualStyleBackColor = True
+        '
+        'ColorChooser
+        '
+        Me.ColorChooser.AnyColor = True
+        '
+        'GraphicForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.EraseButton)
+        Me.Controls.Add(Me.ColorButton)
+        Me.Name = "GraphicForm"
+        Me.Text = "Richard's Sketchy Etch-and-Sketch"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ColorButton As Button
+    Friend WithEvents EraseButton As Button
+    Friend WithEvents ColorChooser As ColorDialog
+End Class
---
+++ Etch And Sketch/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class GraphicForm
+    Private Sub GraphicForm_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+        'lineBox()
+    End Sub
+
+    Sub lineBox()
+        Dim graph As Graphics
+        graph = Me.CreateGraphics
+
+        Dim linePen As New Pen(Color.Purple)
+        Dim x2 As Integer = Me.Width - 50
+        Dim y2 As Integer = Me.Height - 50
+        graph.DrawLine(linePen, 10, 10, x2, 10) 'horizontal line
+        graph.DrawLine(linePen, 10, 10, 10, y2) 'vertical line
+
+        graph.DrawLine(linePen, 10, y2, x2, y2) 'horizontal line
+        graph.DrawLine(linePen, x2, 10, x2, y2) 'vertical line
+
+        linePen.Dispose()
+        graph.Dispose()
+
+    End Sub
+
+    Private Sub GraphicForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Dim graph As Graphics
+        graph = Me.CreateGraphics
+
+        Static penColor As Color = Color.DarkViolet
+
+        Dim mousePen As New Pen(penColor)
+        Static startX As Integer
+        Static startY As Integer
+
+        If e.Button.ToString = "Left" Then
+            graph.DrawLine(mousePen, startX, startY, e.X, e.Y)
+        ElseIf e.Button.ToString = "Right" Then
+            graph.Clear(Color.White)
+        ElseIf e.Button.ToString = "Middle" Then
+            ColorChooser.ShowDialog()
+            penColor = ColorChooser.Color
+            mousePen.Color = penColor
+        End If
+
+        startX = e.X
+        startY = e.Y
+
+        mousePen.Dispose()
+        graph.Dispose()
+    End Sub
+
+End Class
