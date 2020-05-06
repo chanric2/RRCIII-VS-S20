c621e0fc83cbfad05d7e00cd7486eed2f4bd7c8d
jekasku chandler <jekasku@gmail.com>
2020-05-06 13:06:37 -0600

    Add in button functions Add in wave forms     border/graticules     sine wave     cos wave TODO Tan wave

--- Etch And Sketch/Form1.Designer.vb
+++ Etch And Sketch/Form1.Designer.vb
@@ -25,12 +25,18 @@ Partial Class GraphicForm
         Me.ColorButton = New System.Windows.Forms.Button()
         Me.EraseButton = New System.Windows.Forms.Button()
         Me.ColorChooser = New System.Windows.Forms.ColorDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ArtPictureBox = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.WaveButton = New System.Windows.Forms.Button()
+        CType(Me.ArtPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'ColorButton
         '
         Me.ColorButton.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ColorButton.Location = New System.Drawing.Point(12, 365)
+        Me.ColorButton.Location = New System.Drawing.Point(12, 12)
         Me.ColorButton.Name = "ColorButton"
         Me.ColorButton.Size = New System.Drawing.Size(235, 73)
         Me.ColorButton.TabIndex = 0
@@ -40,7 +46,7 @@ Partial Class GraphicForm
         'EraseButton
         '
         Me.EraseButton.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.EraseButton.Location = New System.Drawing.Point(553, 365)
+        Me.EraseButton.Location = New System.Drawing.Point(253, 12)
         Me.EraseButton.Name = "EraseButton"
         Me.EraseButton.Size = New System.Drawing.Size(235, 73)
         Me.EraseButton.TabIndex = 1
@@ -51,20 +57,66 @@ Partial Class GraphicForm
         '
         Me.ColorChooser.AnyColor = True
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(735, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'ArtPictureBox
+        '
+        Me.ArtPictureBox.Location = New System.Drawing.Point(6, 19)
+        Me.ArtPictureBox.Name = "ArtPictureBox"
+        Me.ArtPictureBox.Size = New System.Drawing.Size(600, 400)
+        Me.ArtPictureBox.TabIndex = 3
+        Me.ArtPictureBox.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ArtPictureBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 91)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(615, 426)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'WaveButton
+        '
+        Me.WaveButton.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.WaveButton.Location = New System.Drawing.Point(494, 12)
+        Me.WaveButton.Name = "WaveButton"
+        Me.WaveButton.Size = New System.Drawing.Size(235, 73)
+        Me.WaveButton.TabIndex = 5
+        Me.WaveButton.Text = "Trig Waves"
+        Me.WaveButton.UseVisualStyleBackColor = True
+        '
         'GraphicForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1283, 603)
+        Me.Controls.Add(Me.WaveButton)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.EraseButton)
         Me.Controls.Add(Me.ColorButton)
         Me.Name = "GraphicForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Richard's Sketchy Etch-and-Sketch"
+        CType(Me.ArtPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ColorButton As Button
     Friend WithEvents EraseButton As Button
     Friend WithEvents ColorChooser As ColorDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ArtPictureBox As PictureBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents WaveButton As Button
 End Class
--- Etch And Sketch/Form1.vb
+++ Etch And Sketch/Form1.vb
@@ -1,51 +1,193 @@
 ﻿Public Class GraphicForm
+    Dim graph As Graphics
+    Dim penColor As Color = Color.DarkViolet
+    Dim mousePen As New Pen(penColor, 4)
+    Private Sub GraphicForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        graph = ArtPictureBox.CreateGraphics
+    End Sub
+
     Private Sub GraphicForm_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
-        'lineBox()
+        'lineMessage()
+        'DrawString()
     End Sub
 
-    Sub lineBox()
-        Dim graph As Graphics
-        graph = Me.CreateGraphics
+    'Sub lineMessage()
+    '    Dim graph As Graphics
+    '    graph = Me.CreateGraphics
 
-        Dim linePen As New Pen(Color.Purple)
-        Dim x2 As Integer = Me.Width - 50
-        Dim y2 As Integer = Me.Height - 50
-        graph.DrawLine(linePen, 10, 10, x2, 10) 'horizontal line
-        graph.DrawLine(linePen, 10, 10, 10, y2) 'vertical line
+    '    Dim linePen As New Pen(Color.DarkViolet, 4)
+    '    ''Dim x2 As Integer = Me.Width - 50
+    '    ''Dim y2 As Integer = Me.Height - 50
+    '    ''graph.DrawLine(linePen, 10, 10, x2, 10) 'horizontal line
+    '    ''graph.DrawLine(linePen, 10, 10, 10, y2) 'vertical line
 
-        graph.DrawLine(linePen, 10, y2, x2, y2) 'horizontal line
-        graph.DrawLine(linePen, x2, 10, x2, y2) 'vertical line
+    '    ''graph.DrawLine(linePen, 10, y2, x2, y2) 'horizontal line
+    '    ''graph.DrawLine(linePen, x2, 10, x2, y2) 'vertical line
 
-        linePen.Dispose()
-        graph.Dispose()
+    '    'I
+    '    graph.DrawLine(linePen, 0, 100, 100, 100) 'horiz
+    '    graph.DrawLine(linePen, 50, 100, 50, 400) 'vert
+    '    graph.DrawLine(linePen, 0, 400, 100, 400) 'horiz
+    '    'L
+    '    graph.DrawLine(linePen, 150, 100, 150, 400) 'vert
+    '    graph.DrawLine(linePen, 150, 400, 250, 400) 'horiz
+    '    'O
+    '    graph.DrawEllipse(linePen, New Rectangle(300, 100, 200, 300))
+    '    'v
+    '    graph.DrawLine(linePen, 550, 100, 600, 400) 'diag
+    '    graph.DrawLine(linePen, 600, 400, 650, 100) 'diag
+    '    'E
+    '    graph.DrawLine(linePen, 700, 100, 700, 400) 'vert
+    '    graph.DrawLine(linePen, 700, 100, 800, 100) 'horiz
+    '    graph.DrawLine(linePen, 700, 250, 800, 250) 'horiz
+    '    graph.DrawLine(linePen, 700, 400, 800, 400) 'horiz
+    '    'P
+    '    graph.DrawLine(linePen, 850, 130, 850, 400) 'vert
+    '    graph.DrawEllipse(linePen, New Rectangle(850, 100, 150, 150))
+    '    'A
+    '    graph.DrawLine(linePen, 1000, 400, 1050, 100) 'diag
+    '    graph.DrawLine(linePen, 1050, 100, 1100, 400) 'diag
+    '    graph.DrawLine(linePen, 1025, 250, 1075, 250) 'horiz
+    '    'N
+    '    graph.DrawLine(linePen, 1150, 100, 1150, 400) 'horiz
+    '    graph.DrawLine(linePen, 1150, 100, 1250, 400) 'diag
+    '    graph.DrawLine(linePen, 1250, 100, 1250, 400) 'horiz
 
-    End Sub
+    '    linePen.Dispose()
+    '    graph.Dispose()
 
-    Private Sub GraphicForm_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
-        Dim graph As Graphics
-        graph = Me.CreateGraphics
+    'End Sub
+    'Public Sub DrawString()
+    '    Dim formGraphics As System.Drawing.Graphics = Me.CreateGraphics()
+    '    Dim drawString As String = TextBox1.Text
+    '    TextBox1.Text = "I LOVE PAN"
+    '    Dim drawFont As New System.Drawing.Font("Times New Roman", 175)
+    '    Dim drawBrush As New _
+    '       System.Drawing.SolidBrush(System.Drawing.Color.Purple)
+    '    Dim x As Single = -55.0
+    '    Dim y As Single = 450.0
+    '    Dim drawFormat As New System.Drawing.StringFormat
+    '    formGraphics.DrawString(drawString, drawFont, drawBrush,
+    '        x, y, drawFormat)
 
-        Static penColor As Color = Color.DarkViolet
+    '    drawFont.Dispose()
+    '    drawBrush.Dispose()
+    '    formGraphics.Dispose()
+    'End Sub
 
-        Dim mousePen As New Pen(penColor)
+    Private Sub GraphicForm_MouseMove(sender As Object, e As MouseEventArgs) Handles ArtPictureBox.MouseMove
+
         Static startX As Integer
         Static startY As Integer
 
-        If e.Button.ToString = "Left" Then
+        If e.Button.ToString <> "None" Then
             graph.DrawLine(mousePen, startX, startY, e.X, e.Y)
-        ElseIf e.Button.ToString = "Right" Then
-            graph.Clear(Color.White)
-        ElseIf e.Button.ToString = "Middle" Then
-            ColorChooser.ShowDialog()
-            penColor = ColorChooser.Color
-            mousePen.Color = penColor
         End If
 
         startX = e.X
         startY = e.Y
 
-        mousePen.Dispose()
-        graph.Dispose()
+        Me.Text = "X: " & e.X & " Y: " & e.Y
+    End Sub
+
+    Private Sub ColorButton_Click(sender As Object, e As EventArgs) Handles ColorButton.Click
+        ColorChooser.ShowDialog()
+        penColor = ColorChooser.Color
+        mousePen.Color = penColor
+
+    End Sub
+
+    Private Sub EraseButton_Click(sender As Object, e As EventArgs) Handles EraseButton.Click
+
+        Dim position1 As Integer = Me.Top
+        For i = 1 To 3
+            For shake = 1 To 10000000
+                'move to bottom
+                If shake <= 5000000 Then
+                    'move to up
+                    Me.Top = position1 - 300
+                Else
+                    Me.Top = position1
+                End If
+            Next
+        Next
+
+        graph.Clear(BackColor)
+
+
+    End Sub
+
+    Private Sub WaveButton_Click(sender As Object, e As EventArgs) Handles WaveButton.Click
+        Dim gratPen As New Pen(Color.Black, 2)
+
+        graph.Clear(BackColor)
+        'border
+        Dim xWidth As Integer = ArtPictureBox.Width
+        Dim yHeight As Integer = ArtPictureBox.Height
+        graph.DrawLine(gratPen, 0, 0, xWidth, 0) 'horizontal line
+        graph.DrawLine(gratPen, 0, 0, 0, yHeight) 'vertical line
+
+        graph.DrawLine(gratPen, 0, yHeight, xWidth, yHeight) 'horizontal line
+        graph.DrawLine(gratPen, xWidth, 0, xWidth, yHeight) 'vertical line
+
+        'graticules vertical
+        For x1 = 60 To 540 Step 60
+            For y1 = 5 To 400 Step 10
+                graph.DrawLine(gratPen, x1, y1, x1, y1 + 5)
+            Next
+        Next
+        'graticules horizontal
+        For y2 = 40 To 360 Step 40
+            For x2 = 5 To 600 Step 10
+                graph.DrawLine(gratPen, x2, y2, x2 + 5, y2)
+            Next
+        Next
+
+        Dim sinePen As New Pen(Color.DarkCyan, 4)
+        Dim cosPen As New Pen(Color.Crimson, 4)
+        Dim tanPen As New Pen(Color.LemonChiffon, 4)
+        Dim beginX1 As Integer
+        Dim beginY1 As Integer = 200
+        Dim beginX2 As Integer
+        Dim beginY2 As Integer = 160
+        Dim beginX3 As Integer
+        Dim beginY3 As Integer = 200
+        Dim sineX As Integer
+        Dim sineY As Integer
+        Dim cosX As Integer
+        Dim cosY As Integer
+        Dim tanX As Integer
+        Dim tanY As Integer
+
+        'sin Wave
+        For x = 1 To 600 Step 2
+            sineX = x
+            sineY = 200 - 40 * (Math.Sin((x / 300) * 2 * Math.PI))
+            graph.DrawLine(sinePen, beginX1, beginY1, sineX, sineY)
+            beginX1 = sineX
+            beginY1 = sineY
+        Next
+        'cos wave
+        For y = 1 To 600 Step 2
+            cosX = y
+            cosY = 200 - 40 * (Math.Cos((y / 300) * 2 * Math.PI))
+            graph.DrawLine(cosPen, beginX2, beginY2, cosX, cosY)
+            beginX2 = cosX
+            beginY2 = cosY
+        Next
+        'tan wave
+
+        'If tanY <= -1 Then
+        '    tanY = -1
+        'ElseIf tanY >= 401 Then
+        '    tanY = 401
+        'End If
+        'If tanX = "set x" Then
+        '    graph.DrawLine(tanPen, beginX, beginY, tanX, tanY)
+        'Else
+        '    graph.DrawLine(tanPen, beginX, beginY, tanX, tanY)
+        'End If
+
     End Sub
 
 End Class
